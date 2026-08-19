cf69d537c026e7a98b7853526fb8c05cd8220971
RenYiling <RenYiling@users.noreply.github.com>
2026-08-19 23:32:16 +0800

    Keep count_it submodule focused on counting core

--- realtime/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Realtime camera counting application."""
-
-from .config import AppConfig, load_config
-from .counter import StreamingCounter
-from .service import MeasurementService
-
-__all__ = ["AppConfig", "MeasurementService", "StreamingCounter", "load_config"]
